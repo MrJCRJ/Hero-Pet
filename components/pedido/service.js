a2ee9ff45a1ecff87186a5684969819653a9e320
Jose Cicero <mr.jociriju@gmail.com>
2025-10-02 18:30:04 -0300

    fix: corrigir formatação de exportação no serviço de pedidos

--- components/pedido/service.js
+++ components/pedido/service.js
@@ -2,5 +2,5 @@
 // para a nova localização em 'components/pedidos/service'.
 // TODO: Atualizar testes para importar diretamente de 'components/pedidos/service'
 // e remover este arquivo após a transição.
-export * from '../pedidos/service';
-export { default } from '../pedidos/service';
+export * from "../pedidos/service";
+export { default } from "../pedidos/service";
